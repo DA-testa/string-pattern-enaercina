4a86ef6a537f0ee55f961785bfd701de9484ef51
enaercina <125195785+enaercina@users.noreply.github.com>
2023-03-31 19:21:32 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,36 +1,41 @@
 def read_input():
-    # Read two lines of input: the pattern and the text
-    pattern = input().rstrip()
-    text = input().rstrip()
-    return pattern, text
+    # Input reading function
+    return input().rstrip(), input().rstrip()
+
 
 def print_occurrences(output):
-    # Print the positions of occurrences
+    # Output printing function
     print(' '.join(map(str, output)))
 
+
 def get_occurrences(pattern, text):
-    # Define constants for the Rabin-Karp algorithm
-    p = 31
-    m = 10**9 + 9
-    # Calculate the hash of the pattern and the first substring of the text
-    pattern_hash = 0
-    substring_hash = 0
-    p_power = 1
+    # Rabin-Karp algorithm implementation to find all occurrences of a pattern in a text
+    result = []
+    p = 10**9 + 7  # prime number for hashing
+    x = 263  # base number for hashing
+
+    # Hash the pattern and the first window of the text
+    p_hash = 0
+    t_hash = 0
+    x_pow = 1
     for i in range(len(pattern)):
-        pattern_hash = (pattern_hash + (ord(pattern[i]) - ord('a') + 1) * p_power) % m
-        substring_hash = (substring_hash + (ord(text[i]) - ord('a') + 1) * p_power) % m
-        p_power = (p_power * p) % m
-    # Slide the window over the text and compare hash values
-    occurrences = []
+        p_hash = (p_hash + ord(pattern[i]) * x_pow) % p
+        t_hash = (t_hash + ord(text[i]) * x_pow) % p
+        x_pow = (x_pow * x) % p
+
+    # Compare the hash of the pattern and the hash of the current window in the text
     for i in range(len(text) - len(pattern) + 1):
-        if pattern_hash == substring_hash:
-            if pattern == text[i:i+len(pattern)]:
-                occurrences.append(i)
+        if p_hash == t_hash:
+            if text[i:i+len(pattern)] == pattern:
+                result.append(i)
+
+        # Calculate the hash of the next window in the text
         if i < len(text) - len(pattern):
-            substring_hash = (substring_hash - (ord(text[i]) - ord('a') + 1) * p_power) % m
-            substring_hash = (substring_hash * p + (ord(text[i+len(pattern)]) - ord('a') + 1)) % m
-            substring_hash = (substring_hash + m) % m
-    return occurrences
+            t_hash = (x * (t_hash - ord(text[i]) * x_pow) + ord(text[i+len(pattern)])) % p
+            if t_hash < 0:
+                t_hash += p
+    return result
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
